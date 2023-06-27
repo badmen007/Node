7381e96077c298ffea49d473aa7202401d1cf3fe
badmen007 <1572226525@qq.com>
2023-06-27 22:18:35 +0800

    feat: commonjs简单实现

--- 3.loop-module/2.js
+++ 3.loop-module/2.js
@@ -3,7 +3,7 @@
 // node是单线程的，但是它是异步的，它的异步是通过事件循环来实现的
 // node的特点：非阻塞I/O，事件驱动，单线程，跨平台，轻量高效
 // node的应用场景：高并发，I/O密集型，分布式应用，前端工程化
-//
+
 // 异步非阻塞，同步阻塞
 
 
@@ -32,3 +32,31 @@
 // 1. 每个js文件都是一个模块
 // 2. 通过module.exports导出模块
 // 3. 通过require引入模块
+
+// 如果不带路径会被识别成原生模块或者第三方模块
+// 内部会自动帮我们添加 .js .json 后缀
+// 同步导入模块
+const r = require('./a')
+console.log(r)
+
+// 1.如何读取文件 path(采用的是绝对路径)
+const path = require('path')
+const fs = require('fs')
+console.log(path.join(__dirname, 'a', 'b')) // 指示拼接
+console.log(path.resolve(__dirname, 'a', 'b')) // 指示解析
+
+// 上面解析的路径是一样的，但是join只是拼接，resolve会解析出一个绝对路径
+// 在一定情况下两个解析的 结果是一样的，但是当遇到/的时候，resolve认为是根路径，join只是拼接
+
+console.log(path.extname('a.min.js')) // 获取文件的扩展名
+
+// 读取文件如果文件不存在会报错
+fs.readFileSync(path.resolve(__dirname, 'test.md', 'utf8')) // 同步读取文件
+console.log(fs.existsSync(path.resolve(__dirname, 'test.md'))) // 判断文件是否存在
+
+// eval 
+// new Function 
+
+const vm = require('vm')
+
+vm.runInThisContext('console.log(a)') // 在当前上下文中执行代码
---
+++ 3.loop-module/3.js
@@ -0,0 +1,64 @@
+const fs = require('fs')
+const path = require('path')
+const vm = require('vm')
+function Module(id) {
+    this.id = id
+    this.exports = {}
+}
+Module.prototype.load = function() {
+    const ext = path.extname(this.id)
+    Module._extensions[ext](this)
+}
+Module._extensions = {
+    '.js'(module){
+        const content = fs.readFileSync(module.id, 'utf8')
+        const fn = vm.compileFunction(content, ['exports', 'module', 'require', '__dirname', '__filename'])
+        const exports = module.exports
+        let thisValue = exports
+        const require = req
+        const filename = module.id
+        const dirname = path.dirname(filename)
+        console.log(fn.toString())
+        Reflect.apply(fn, thisValue, [exports, module, require, dirname, filename])
+    },
+    '.json'(module){
+        const content = fs.readFileSync(module.id, 'utf8')
+        module.exports = JSON.parse(content)
+    }
+}
+Module._resolveFilename = function(id) {
+    const filename = path.resolve(__dirname, id)
+    if (fs.existsSync(filename)) {
+        return filename
+    }
+    const keys = Object.keys(Module._extensions)
+    for (let i = 0; i < keys.length; i++) {
+        const ext = keys[i]
+        const filename = path.resolve(__dirname, id + ext)
+        if (fs.existsSync(filename)) {
+            return filename
+        }
+    }
+    throw new Error('module not found')
+}
+Module._cache = {} // 缓存
+
+function req(id) {
+    const filename = Module._resolveFilename(id)
+    let existsModule = Module._cache[filename]
+    if (existsModule) {
+        return existsModule.exports
+    }
+    const module = new Module(filename)
+    Module._cache[filename] = module
+    module.load()
+    return module.exports
+}
+
+
+const a = req('./a')
+console.log(a)
+
+// 1. 先通过id来解析出一个绝对路径 拼接括扩展名
+// 2. 读出文件的内容，创造一个沙箱环境执行 vm.compilerFunction Reflect.apply执行 
+// 3. 模块的缓存 通过文件名来缓存
